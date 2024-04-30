c0d84314d7c0351df86d9db52c704acedec6d577
Leon Richardson <133119460+Supaproducer901@users.noreply.github.com>
2024-04-30 16:31:24 -0400

    Add files via upload

---
+++ house cleaning roulette.py
@@ -0,0 +1,6 @@
+names= ["Leon","Ashley","Carmen","Keiron","Will","Diamond"]
+import random
+num_items = len(names)
+random_choice=random.randint(0,num_items-1)
+person_who_will_pay=names[random_choice]
+print(person_who_will_pay + " is the cleaner today")
